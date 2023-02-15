5f35d86ad588e4600238232b3940ac968894d932
zainul-bashar <120213051+zainul-bashar@users.noreply.github.com>
2023-02-15 13:38:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
+var input=document.getElementById("guess");
+var btn=document.getElementById("btn");
+btn.addEventListener("click",randomNumGenerator;
+function randomNumGenerator(){
+	var random=Math.floor(Math.random()*41)-20;
+	document.getElementById("num").innerText=random;
+	guessNum()
+	print()
+}
+function guessNum(){
+	var inputValue=Number(input.value);
+}
+function print(){
+	if(Math.abs(random-inputValue)<=5){
+		document.getElementByid("respond").innerText="hot";
+	}
+	else{
+		document.getElementByid("respond").innerText="cold";
+	}
+}
+
 
